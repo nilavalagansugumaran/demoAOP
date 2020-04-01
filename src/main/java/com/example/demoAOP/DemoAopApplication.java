255476ac80253ab5082a16b7d10c3211bee80dfb
Nilavalagan Sugumaran <>
2020-04-01 14:00:20 +0100

    JOIN POINTS ADDED

--- src/main/java/com/example/demoAOP/DemoAopApplication.java
+++ src/main/java/com/example/demoAOP/DemoAopApplication.java
@@ -10,6 +10,21 @@ public class DemoAopApplication {
 	public static void main(String[] args) {
 
 		ApplicationContext context = SpringApplication.run(DemoAopApplication.class, args);
+
+		//Join points - are the method calls
+		context.getBean(MyService.class).sayGoodBye();
+		context.getBean(MyService.class).getComponenet();
+		context.getBean(MyService.class).sayHello("nila - MyService");
+
+		context.getBean(MyOtherService.class).sayHello("nila - MyOtherService");
+
+
+		context.getBean(MyComponent.class).sayGoodBye();
+		context.getBean(MyComponent.class).getComponenet();
+
+		context.getBean(MyOtherComponent.class).sayHello("nila - MyOtherService");
+		context.getBean(MyOtherComponent.class).getComponenet();
+
 	}
 
 }
